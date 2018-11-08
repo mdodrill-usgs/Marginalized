158b3419fe0c061990428c26065bc3b09abb38ff
Mike <mdodrill@usgs.gov>
2018-11-08 09:34:49 -0700

    Working OpenBUGS Discrete Time Model

--- Application_1/OpenBUGS_Discrete_Time.R
+++ Application_1/OpenBUGS_Discrete_Time.R
@@ -4,7 +4,8 @@
 #  Discrete OpenBUGS version - fixed time effects
 #
 #  Notes:
-#  * 
+#  * OpenBUGS will crash with indexing of omega and rho like other CJS examples,
+#    see note below. Same issue as WinBUGS. 
 #
 #  To do: 
 #  * 
@@ -30,7 +31,7 @@ indf <- apply(CH, 1, get.first)
 Z = known.state.cjs(CH)
 
 #-----------------------------------------------------------------------------#
-sink("WinBUGS_Discrete_Time.WinBUGS")
+sink("OpenBUGS_Discrete_Time.OpenBUGS")
 cat("
 model{
   
@@ -38,27 +39,41 @@ model{
     phi[t] ~ dunif(0,1)
     p[t] ~ dunif(0,1)
     
-    omega[1,1,t] <- phi[t]
-    omega[1,2,t] <- 1 - phi[t]
-    omega[2,1,t] <- 0
-    omega[2,2,t] <- 1
+    # OpenBUGS crashes if indexed with t as the third dimension
+    # omega[1,1,t] <- phi[t]
+    # omega[1,2,t] <- (1 - phi[t])
+    # omega[2,1,t] <- 0
+    # omega[2,2,t] <- 1
+    # 
+    # rho[1,1,t] <- p[t]
+    # rho[1,2,t] <- (1 - p[t])
+    # rho[2,1,t] <- 0
+    # rho[2,2,t] <- 1
     
-    rho[1,1,t] <- p[t]
-    rho[1,2,t] <- 1 - p[t]
-    rho[2,1,t] <- 0
-    rho[2,2,t] <- 1
+    omega[1,t,1] <- phi[t]
+    omega[1,t,2] <- (1 - phi[t])
+    omega[2,t,1] <- 0
+    omega[2,t,2] <- 1
+    
+    rho[1,t,1] <- p[t]
+    rho[1,t,2] <- (1 - p[t])
+    rho[2,t,1] <- 0
+    rho[2,t,2] <- 1
   }
   
   for(i in 1:NY){
     Z[i,indf[i]] <- 1
     
     for(t in indf[i]:Nint){
-      Z[i,(t + 1)] ~ dcat(omega[Z[i, t], , t])
-      Y[i,(t + 1)] ~ dcat(rho[Z[i, (t + 1)], , t])
+      # Z[i,(t + 1)] ~ dcat(omega[Z[i, t], , t])
+      # Y[i,(t + 1)] ~ dcat(rho[Z[i, (t + 1)], , t])
+      
+      Z[i,(t+1)] ~ dcat(omega[Z[i, t], t, ])
+      Y[i,(t+1)] ~ dcat(rho[Z[i, (t+1)], t, ])
     }
   }
 }
-    ", fill = TRUE)
+", fill = TRUE)
 sink()    
 
 #-----------------------------------------------------------------------------#
@@ -70,7 +85,7 @@ nt <- 1
 nb <- 5
 
 t1 <- proc.time()
-JD.out <- R2OpenBUGS::bugs(JD.data, inits = NULL, JD.par, "WinBUGS_Discrete_Time.WinBUGS",
+JD.out <- R2OpenBUGS::bugs(JD.data, inits = NULL, JD.par, "OpenBUGS_Discrete_Time.OpenBUGS",
                n.chains = 3, n.iter = ni, n.thin = nt, n.burnin = nb)
 t2 <- proc.time()
 
