6fc84390fa1f5925e64d74e1eb74de902586004f
Dodrill <mdodrill@usgs.gov>
2018-12-18 11:55:28 -0700

    added 'organize' function

--- Functions.R
+++ Functions.R
@@ -317,5 +317,60 @@ run.times.2 = function(fit.list){
 
 # run.times.2(fit.list)  
 
+#-----------------------------------------------------------------------------#
+
+
+organize = function(fit, par.name, mcmc.out = FALSE){
+  require(ggmcmc)
+  
+  # JAGS
+  if(class(fit)[1] == "rjags"){
+    if(mcmc.out == FALSE){
+      tmp = coda::mcmc.list(lapply(1:fit$model$nchain(), function(x) coda::mcmc(fit$BUGSoutput$sims.array[,x,])))
+      
+      f1 = ggs(tmp, family = par.name)
+      
+    } else {
+      
+      f1 = coda::mcmc.list(lapply(1:fit$model$nchain(), function(x) coda::mcmc(fit$BUGSoutput$sims.array[,x,])))
+    }
+  }
+  
+  # JAGS Parallel
+  if(class(fit)[1] == "rjags.parallel"){
+    if(mcmc.out == FALSE){
+      tmp = coda::mcmc.list(lapply(1:3, function(x) coda::mcmc(fit$BUGSoutput$sims.array[,x,])))
+      
+      f1 = ggs(tmp, family = par.name)
+      
+    } else {
+      
+      f1 = coda::mcmc.list(lapply(1:3, function(x) coda::mcmc(fit$BUGSoutput$sims.array[,x,])))
+    }
+  }
+  
+  # for a stan object
+  if(any(class(fit) == "stanfit")){
+    if(mcmc.out == FALSE){
+      # looks like gss works directly with stan objects now...
+      f1 <- ggs(fit, family = par.name)
+      # used to do this... but with the new 'family' argument 
+      # tmp <- coda::mcmc.list(lapply(1:ncol(fit), function(x) coda::mcmc(as.array(fit)[,x,])))
+      # f1 <- ggs(tmp, family = par.name)
+      
+    } else {
+      
+      f1 = coda::mcmc.list(lapply(1:ncol(fit), function(x) coda::mcmc(as.array(fit)[,x,])))
+      
+    }
+  }
+  
+  return(f1)
+}
+
+
+# organize(JD.out, par.name = "p")
+# organize(SM.c, par.name = "p")
+
 
 ###############################################################################
