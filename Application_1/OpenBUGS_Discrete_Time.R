cc990c1f588f64156e2373e0a27bf49f195b93ae
Dodrill <mdodrill@usgs.gov>
2018-12-18 13:00:13 -0700

    Changed directory/source at start

--- Application_1/OpenBUGS_Discrete_Time.R
+++ Application_1/OpenBUGS_Discrete_Time.R
@@ -11,11 +11,15 @@
 #  * 
 #
 ###############################################################################
-setwd(paste0(getwd(), '/Application_1'))
 library(R2OpenBUGS)
+source(paste0(getwd(),"/Functions.R"), chdir = F)
 
-source("RBT_Functions.R", chdir = F)
+setwd(paste0(getwd(), '/Application_1'))
 
+data.dir = paste0(getwd(), "/Data")
+CH = as.matrix(read.table(file = paste0(data.dir, "/RBT_Capture_History.txt"),
+                          header = FALSE, sep = "\t"))
+#-----------------------------------------------------------------------------#
 # format data for model fitting
 Y = CH
 
@@ -96,7 +100,7 @@ print(OD.out, digits = 3)
 library(foreach)
 library(doParallel)
 
-n.core = 30
+n.core = 10
 
 cl1 = makeCluster(n.core) # number of cores you want to use
 registerDoParallel(cl1)
@@ -108,11 +112,12 @@ all.t1 = proc.time()
 n.runs = 10
 
 # my.n.iter = c(10, 15)
-my.n.iter = seq(0,10000,500)[- 1]
+# my.n.iter = seq(0,10000,500)[- 1]
+my.n.iter = seq(11000,20000,1000)
 # my.n.iter = rep(500, 1)
 
 # my.n.iter = my.n.iter[1:10]
-my.n.iter = my.n.iter[11:20]
+# my.n.iter = my.n.iter[11:20]
 
 big.fit.list = list()
 
@@ -174,10 +179,10 @@ length(all.out)
 
 tmp = run.times(all.out)
 
-all.Open.d.time.1 = all.out
+all.Open.d.time.3 = all.out
 
-rm(list=setdiff(ls(), "all.Open.d.time.1"))
+rm(list=setdiff(ls(), "all.Open.d.time.3"))
 
-save.image("U:/Desktop/Fish_Git/Marginalized/Application_1/working_Runs/Open_D_Time.RData")
+save.image("U:/Desktop/Fish_Git/Marginalized/Application_1/working_Runs/Open_D_Time_3.RData")
 
 #-----------------------------------------------------------------------------#
