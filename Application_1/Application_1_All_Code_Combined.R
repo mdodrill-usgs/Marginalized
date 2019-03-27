c9aa53f0d74e1d511e720697b184c69b657c71ea
Mike <mdodrill@usgs.gov>
2019-03-27 11:46:09 -0700

    Added WD

--- Application_1/Application_1_All_Code_Combined.R
+++ Application_1/Application_1_All_Code_Combined.R
@@ -339,3 +339,94 @@ OM.out <- R2OpenBUGS::bugs(OM.data, inits = NULL, OM.par, "OpenBUGS_Marginalized
                n.chains = 3, n.iter = ni, n.thin = nt)
 
 #-----------------------------------------------------------------------------#
+###############################################################################
+#                                                                     Spring 19
+#  Fitting a multi-state version of a CJS model to the RBT data 
+#  Discrete WinBUGS version - fixed time effects
+#
+#  Notes:
+#  * WinBUGS will crash with indexing of omega and rho like other CJS examples,
+#    see note below.
+#  * Need to set directory for data
+#  * Need to supply JAGS settings
+#
+###############################################################################
+library(R2WinBUGS)
+
+# data.dir = paste0(getwd(), "/Data")
+CH = as.matrix(read.table(file = paste0(data.dir, "/RBT_Capture_History.txt"),
+                          header = FALSE, sep = "\t"))
+#-----------------------------------------------------------------------------#
+# format data for model fitting
+Y = CH
+
+Y[Y[,] == 0] = 2
+
+NY = nrow(Y)          # number of capture histories 
+Nint = ncol(Y) - 1    # number of intervals
+
+# Create vector with occasion of marking
+get.first <- function(x) min(which(x != 0))
+indf <- apply(CH, 1, get.first)
+
+Z = known.state.cjs(CH)
+
+#-----------------------------------------------------------------------------#
+sink("WinBUGS_Discrete_Time.WinBUGS")
+cat("
+model{
+  
+  for(t in 1:Nint){ 
+    phi[t] ~ dunif(0,1)
+    p[t] ~ dunif(0,1)
+    
+    # WinBUGS crashes if indexed with t as the third dimension
+    # omega[1,1,t] <- phi[t]
+    # omega[1,2,t] <- (1 - phi[t])
+    # omega[2,1,t] <- 0
+    # omega[2,2,t] <- 1
+    # 
+    # rho[1,1,t] <- p[t]
+    # rho[1,2,t] <- (1 - p[t])
+    # rho[2,1,t] <- 0
+    # rho[2,2,t] <- 1
+    
+    omega[1,t,1] <- phi[t]
+    omega[1,t,2] <- (1 - phi[t])
+    omega[2,t,1] <- 0
+    omega[2,t,2] <- 1
+    
+    rho[1,t,1] <- p[t]
+    rho[1,t,2] <- (1 - p[t])
+    rho[2,t,1] <- 0
+    rho[2,t,2] <- 1
+  }
+  
+  for(i in 1:NY){
+    Z[i,indf[i]] <- 1
+    
+    for(t in indf[i]:Nint){
+      # Z[i,(t + 1)] ~ dcat(omega[Z[i, t], , t])
+      # Y[i,(t + 1)] ~ dcat(rho[Z[i, (t + 1)], , t])
+      
+      Z[i,(t+1)] ~ dcat(omega[Z[i, t], t, ])
+      Y[i,(t+1)] ~ dcat(rho[Z[i, (t+1)], t, ])
+    }
+  }
+}
+    ", fill = TRUE)
+sink()    
+
+#-----------------------------------------------------------------------------#
+WD.data <- list(NY = NY, Nint = Nint, Y = Y, indf = indf, Z = Z)
+WD.par <- c('phi', 'p')
+
+# ni <- 1000
+# nt <- 1
+# nb <- 500
+
+# be explicit on the call to 'bugs', as the names are same for both R2WinBUGS and R2OpenBUGS
+WD.out <- R2WinBUGS::bugs(WD.data, inits = NULL, WD.par, "WinBUGS_Discrete_Time.WinBUGS",
+                          n.chains = 3, n.iter = ni, n.thin = nt, n.burnin = nb, debug = FALSE)
+
+#-----------------------------------------------------------------------------#
