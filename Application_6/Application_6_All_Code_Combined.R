adba0c32e5eaa0dc26e02a977d3e7ac59d366f64
Dodrill <mdodrill@usgs.gov>
2019-04-02 14:55:38 -0700

    Added combined file

---
+++ Application_6/Application_6_All_Code_Combined.R
@@ -0,0 +1,13 @@
+###############################################################################
+#                                                                     Spring 19
+#  Fitting an Integrated Population Model to Brown Trout Data
+#  Discrete JAGS version 
+#
+#  Notes:
+#  * The model runs from the fall of 2000 to fall of 2017 on a seasonal basis
+#  * We define three size states based on total length in mm 
+#    - 0 - 200; 200 - 350; 350 +
+#  * Need to set directory for data
+#  * Need to supply JAGS/WinBUGS/Stan settings
+#
+###############################################################################
